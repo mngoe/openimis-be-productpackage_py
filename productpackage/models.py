70390ba710eb7f743dd7e5d6f30866fe00546f93
20GHhub19 <58400969+20GHhub19@users.noreply.github.com>
2022-06-03 16:30:43 +0100

    files of productpackage updated 03_06_22

--- productpackage/models.py
+++ productpackage/models.py
@@ -4,8 +4,9 @@ from product import models as product_models
 
 # Create your models here.
 class ProductPackage(product_models.Product):
-    package = models.CharField(db_column="ProductPackage", max_length=100, blank=True, null=True)
+    package = models.CharField( db_column="ProductPackage", max_length=100, blank=True, null=True)
 
-class Meta:
-    managed = False
-    db_table = "tblProduct"
+    class Meta:
+        managed = False
+        db_table = "tblProduct"
+
