fb6a0528423fffd437e115da0e5bfd5c2a1ba5b2
20GHhub19 <58400969+20GHhub19@users.noreply.github.com>
2022-06-03 11:29:19 +0100

    start writing code to add a field into product module (tblProduct) 03_06_22

--- productpackage/models.py
+++ productpackage/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
+from product import models as product_models
 
 # Create your models here.
+class ProductPackage(product_models.Product):
+    package = models.CharField(db_column="ProductPackage", max_length=100, blank=True, null=True)
+
+class Meta:
+    managed = False
+    db_table = "tblProduct"
