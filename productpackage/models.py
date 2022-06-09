9425ce3c7f950a00c67cc1ad30ddce0594195e9d
20GHhub19 <58400969+20GHhub19@users.noreply.github.com>
2022-06-09 20:38:18 +0100

    models of module productpackage updated on 09_08_22

--- productpackage/models.py
+++ productpackage/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.db.models import Count
 from product import models as product_models
+#from sql_util.tests.models import Product
+from product.models import Product
+
 from productpackage import models as productpackage_models
 from medical import models as medical_models
 
@@ -12,10 +16,22 @@ class ProductPackage(models.Model):
     PackagePrice = models.DecimalField(db_column="PackagePrice", max_digits=18, decimal_places=2,  blank=True, null=True)
     PackageCheckBox = models.BooleanField(default= True)
 
+    @classmethod
+    def delete_productpackagerow(cls, id_productpackagerow):  # delete a row in productPackage table
+        cls.objects.find(idSCP=id_productpackagerow).delete()
+
 
     class Meta:
         managed = True
         db_table ='tblProducPackage'
+
+    ### CRUD for Productpackage model
+
+    # Define a function to create an entry
+    #@classmethod
+    #def createproductpackage(cls):
+
+
 
 
 class ProductContainedPackage(models.Model):
@@ -27,9 +43,19 @@ class ProductContainedPackage(models.Model):
     PCPPrice = models.DecimalField(db_column="PackagePrice", max_digits=18, decimal_places=2,  blank=True, null=True)
 
 
+    @classmethod
+    def sum_quantityproduct(cls):
+        return cls.objects.all().aggregate(sum('PCPQuantity'))
+
+    @classmethod
+    def delete_productcontainedpackagerow(cls, id_productrow ): #delete a row in ProductContainedPackage table
+        cls.objects.find(idPCP = id_productrow).delete()
+
+
     class Meta:
         managed = True
         db_table ='tblProductContainedPackage'
+
 
 
 class ServiceContainedPackage(models.Model):
@@ -40,8 +66,33 @@ class ServiceContainedPackage(models.Model):
     SCPDate = models.DateTimeField(db_column="PackageDate", blank=True, null=True)
     SCPPrice = models.DecimalField(db_column="PackagePrice", max_digits=18, decimal_places=2, blank=True, null=True)
 
+    @classmethod
+    def sum_quantityservice(cls):  # this fucntion returns the sum of the values of the entries of SCPQuantity field
+        return cls.objects.all().aggregate(sum('SCPQuantity'))
+
+    @classmethod
+    def delete_servicecontainedpackagerow(cls, id_servicerow):  # delete a row in ServiceContainedPackage table
+        try:
+            cls.objects.find(idSCP=id_servicerow).delete()
+        except cls.DoesNotExist:
+            print('Service  with id "%s" does not exist.' % id_servicerow)
 
     class Meta:
         managed = True
         db_table ='tblServiceContainedPackage'
+
+
+
+
+
+def sum_quantity_productservice(): # fucntion that counts the number of entries from the table product and table services
+    return sum(Product.objects.filter(db_column="ProdID").count(),
+               medical_models.Service.objects.filter(db_column ="ServiceID").count())
+
+def delete_productrow(self, id_productrow):  # delete a row in product table
+    try:
+        Product.objects.find(id = id_productrow).delete()
+    except Product.DoesNotExist:
+        print('Product  with id "%s" does not exist.' % id_productrow)
+
 
